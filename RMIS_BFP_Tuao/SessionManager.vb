1b40baed1f4ef6e8b4308445c432ec584773ba00
Glenn-IT <glenard2308@gmail.com>
2026-06-13 00:20:21 +0800

    feat: add SessionManager module to hold logged-in user state across forms

---
+++ RMIS_BFP_Tuao/SessionManager.vb
@@ -0,0 +1,13 @@
+Public Module SessionManager
+
+    Public Username As String = ""
+    Public UserType As String = ""
+    Public UserCode As String = ""
+
+    Public Sub Clear()
+        Username = ""
+        UserType = ""
+        UserCode = ""
+    End Sub
+
+End Module
