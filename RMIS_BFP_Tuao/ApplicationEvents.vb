c55e8b569dc837b9f821a917f4f3d67ada15185e
Glenn-IT <glenard2308@gmail.com>
2026-06-13 00:20:06 +0800

    feat: initialize database on startup and cancel launch on connection error

--- RMIS_BFP_Tuao/ApplicationEvents.vb
+++ RMIS_BFP_Tuao/ApplicationEvents.vb
@@ -1,10 +1,19 @@
-﻿Imports Microsoft.VisualBasic.ApplicationServices
+Imports Microsoft.VisualBasic.ApplicationServices
 
 Namespace My
     Partial Friend Class MyApplication
 
         Private Sub MyApplication_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
-            ' LoginForm is set as the main form via Application.myapp
+            Try
+                DatabaseInitializer.Initialize()
+            Catch ex As Exception
+                MessageBox.Show(
+                    "Could not connect to the database." & Environment.NewLine &
+                    ex.Message & Environment.NewLine & Environment.NewLine &
+                    "Make sure config.txt exists next to the .exe with a valid connection string.",
+                    "Startup Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                e.Cancel = True
+            End Try
         End Sub
 
     End Class
