07146363791dcfa191e57d83ec03c4847d33a866
Glenn-IT <glenard2308@gmail.com>
2026-08-13 22:09:18 +0800

    feat: implement v5.10 - unlock Incident Type & Status filtering on View Records

--- RMIS_BFP_Tuao/Forms/Pages/UcUnderConstruction.vb
+++ RMIS_BFP_Tuao/Forms/Pages/UcUnderConstruction.vb
@@ -1,7 +1,7 @@
 Public Class UcUnderConstruction
     Inherits UserControl
 
-    Public Const CURRENT_VERSION As String = "v5.01"
+    Public Const CURRENT_VERSION As String = "v5.10"
 
     Private _pnlCenter As Panel
     Private _lblEmoji   As Label
